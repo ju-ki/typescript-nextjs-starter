c9fa08af0bbf109a3eec067b02850efaa76c4f37
ju-ki <juki.programming@gmail.com>
2023-11-06 23:26:00 +0900

    ProductCardの一部

---
+++ ec-sample/src/components/organisms/ProductCart/index.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components"
+
+interface ProductCardProps {
+    /**
+     * 商品タイトル
+     */
+    title:string
+    price:number
+    imageUrl:string
+    blurDataUrl:string
+    variant?: "listing" | "small" | "detail"
+}
+
+const ProductCardContainer = styled.div`
+    position:relative;
+`
+
+const ProductCardImageContainer = styled.div`
+    z-index:99;
+`
+
+const ProductCardInfo = styled.div`
+    position:absolute;
+    z-index:100;
+    top:0px;
+    left:0px;
+`
+
+const ProductCard = ({
+    
+})
